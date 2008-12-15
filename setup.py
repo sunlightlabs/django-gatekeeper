bdb476b1bbb302efb81f9ecbe4410c6dab545a80
Jeremy Carbaugh <jcarbaugh@gmail.com>
2008-12-15 15:12:19 -0500

    updated README

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     license='BSD License',
     author='Jeremy Carbaugh',
     author_email='jcarbaugh@sunlightfoundation.com',
-    url='http://code.google.com/p/django-gatekeeper/',
-    download_url='http://code.google.com/p/django-gatekeeper/downloads/list',
+    url='http://github.com/sunlightlabs/django-gatekeeper/',
+    download_url='http://github.com/sunlightlabs/django-gatekeeper/tree/release-0.1',
     packages=['gatekeeper'],
     classifiers=[
         'Development Status :: 4 - Beta',
