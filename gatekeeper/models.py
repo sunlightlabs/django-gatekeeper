5755aee901e94ede03f40c1cffedfb3914f50535
James Turk <james.p.turk@gmail.com>
2009-08-24 18:36:29 -0400

    add reason to ModeratedObject

--- gatekeeper/models.py
+++ gatekeeper/models.py
@@ -15,7 +15,7 @@ STATUS_CHOICES = (
 STATUS_ON_FLAG = getattr(settings, "GATEKEEPER_STATUS_ON_FLAG", None)
 
 class ModeratedObjectManager(models.Manager):
-    
+
     def get_for_instance(self, obj):
         ct = ContentType.objects.get_for_model(obj.__class__)
         try:
@@ -23,16 +23,17 @@ class ModeratedObjectManager(models.Manager):
             return mo
         except ModeratedObject.DoesNotExist:
             pass
-            
+
 class ModeratedObject(models.Model):
-    
+
     objects = ModeratedObjectManager()
-    
+
     timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    
+
     moderation_status = models.IntegerField(choices=STATUS_CHOICES)
     moderation_status_by = models.ForeignKey(User, blank=True, null=True)
     moderation_status_date = models.DateTimeField(blank=True, null=True)
+    moderation_reason = models.CharField(max_length=100, blank=True)
 
     flagged = models.BooleanField(default=False)
     flagged_by = models.ForeignKey(User, blank=True, null=True,
@@ -42,22 +43,23 @@ class ModeratedObject(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
-    
+
     class Meta:
         ordering = ['timestamp']
-    
+
     def __unicode__(self):
         return "[%s] %s" % (self.get_moderation_status_display(), 
                             self.content_object)
-        
+
     def get_absolute_url(self):
         if hasattr(self.content_object, "get_absolute_url"):
             return self.content_object.get_absolute_url()
-        
-    def _moderate(self, status, user):
+
+    def _moderate(self, status, user, reason=None):
         self.moderation_status = status
         self.moderation_status_by = user
         self.moderation_status_date = datetime.datetime.now()
+        self.moderation_reason = reason
         self.save()
         gatekeeper.post_moderation.send(sender=ModeratedObject, instance=self)
 
@@ -73,8 +75,8 @@ class ModeratedObject(models.Model):
         self.save()
         gatekeeper.post_flag.send(sender=ModeratedObject, instance=self)
 
-    def approve(self, user):
-        self._moderate(1, user)
+    def approve(self, user, reason=None):
+        self._moderate(1, user, reason)
 
-    def reject(self, user):
-        self._moderate(-1, user)
+    def reject(self, user, reason=None):
+        self._moderate(-1, user, reason)
