875ddbdb966c5b7bbabd92e09839950e1513be42
Aaron Williamson <aaron@copiesofcopies.org>
2010-04-16 09:13:46 -0400

    Made moderation status editable in moderation queue list, so moderation views are unnecessary.

--- gatekeeper/admin.py
+++ gatekeeper/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from gatekeeper.models import ModeratedObject
 
 class ModeratedObjectAdmin(admin.ModelAdmin):
+    list_display = ('object_name', 'moderation_status',)
+    list_editable = ('moderation_status',)
     list_filter = ['moderation_status','flagged','content_type']
+
+    def object_name(self, obj):
+        return "%s" % obj
 
 admin.site.register(ModeratedObject, ModeratedObjectAdmin)
 
